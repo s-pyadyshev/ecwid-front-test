2d0b378bc1477b9fa51694df28e411c13e20eb36
Sergei Piadyshev <s.pyadyshev@gmail.com>
2020-07-03 12:06:44 +0400

    add drag and drop draft

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React from "react";
-import FileUpload, { fileUploadContext } from "./components/FileUpload";
 import Gallery from "./components/Gallery";
+import DragAndDrop from "./components/DragAndDrop";
+import "./App.scss";
 
 function App() {
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "SET_DROP_DEPTH":
+        return { ...state, dropDepth: action.dropDepth };
+      case "SET_IN_DROP_ZONE":
+        return { ...state, inDropZone: action.inDropZone };
+      case "ADD_FILE_TO_LIST":
+        return { ...state, fileList: state.fileList.concat(action.imgSrc) };
+      default:
+        return state;
+    }
+  };
+
+  const [data, dispatch] = React.useReducer(reducer, {
+    dropDepth: 0,
+    inDropZone: false,
+    fileList: [],
+  });
+
   return (
     <div className="App">
-      <fileUploadContext.Provider>
-        <FileUpload />
+      <div className="container">
         <Gallery />
-      </fileUploadContext.Provider>
+        <DragAndDrop data={data} dispatch={dispatch} />
+        <ol className="dropped-files">
+          {data.fileList.map((file, index) => {
+            return (
+              <li key={index}>
+                <img src={file} />
+              </li>
+            );
+          })}
+        </ol>
+      </div>
     </div>
   );
 }
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,20 +1,115 @@
-import React, { useContext, useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import "./style.scss";
+
+const Image = (props) => {
+  const { url, width, height, imageWidth } = props;
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  return (
+    <>
+      {loaded ? (
+        <img
+          src={url}
+          width={width}
+          height={height}
+          alt=".some alt text"
+          style={{ width: imageWidth }}
+        />
+      ) : (
+        <div style={{ width: imageWidth }}>...loading</div>
+      )}
+    </>
+  );
+};
 
 const Gallery = () => {
-  // const images = useContext(galleryImagesContext);
-  // const { images } = props;
-  useEffect(() => {
-    // console.log(images);
-  }, [useContext]);
+  // input file
+  const [images, setImages] = useState([]);
+  // input text
+  const [imageInput, setImageInput] = useState("");
+
+  const inputRef = useRef();
+  const imagesPerRow = 5;
+  const containerMaxWidth = 860;
+  const imageWidth = containerMaxWidth / imagesPerRow - 10 + "px";
+
+  let fileReader;
+  let galleryImages = [];
+
+  const handleFileRead = (e) => {
+    const content = JSON.parse(fileReader.result).galleryImages;
+    galleryImages = content;
+  };
+
+  const handleFileChosen = (file) => {
+    fileReader = new FileReader();
+    fileReader.onloadend = handleFileRead;
+    fileReader.readAsText(file);
+  };
+
+  // загрузить картинку
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setImages([...images, ...galleryImages]);
+  };
+
+  // вставить картинку ссылкой
+  const handleInputText = (event) => {
+    setImageInput(event.target.value);
+  };
+
+  const handleImageAdd = (event) => {
+    event.preventDefault();
+    setImages([...images, { url: imageInput }]);
+  };
+
+  // удалить картинку
+  const handleImageDelete = (imageIndex) => {
+    setImages([...images.filter((image, index) => index !== imageIndex)]);
+  };
+
   return (
-    <ul>
-      {/* {images &&
-        images.map((image, index) => (
+    <div>
+      <form>
+        <div>
+          <input type="text" onChange={handleInputText} />
+          <button onClick={handleImageAdd}>Добавить картинку</button>
+        </div>
+        <div>
+          <input
+            type="file"
+            id="upload"
+            ref={inputRef}
+            onChange={(e) => handleFileChosen(e.target.files[0])}
+          />
+          <label htmlFor="upload">Загрузить</label>
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
+      </form>
+
+      <ul className="gallery__list">
+        {images.map((image, index) => (
           <li key={index}>
-            <img src={image.url} width={image.width} height={image.height} />
+            <Image
+              url={image.url}
+              width={image.width}
+              height={image.height}
+              imageWidth={imageWidth}
+            />
+            <button
+              className="gallery__list-item-del"
+              onClick={() => handleImageDelete(index)}
+            >
+              remove
+            </button>
           </li>
-        ))} */}
-    </ul>
+        ))}
+      </ul>
+    </div>
   );
 };
 
